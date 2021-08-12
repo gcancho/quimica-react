0727e5efd70b9b7d522f28b51b13a0fab30a98d1
gcancho <giovannicb19@gmail.com>
2021-08-11 19:55:41 -0500

    Certificado en nosotros

--- src/componentes/Nosotros.js
+++ src/componentes/Nosotros.js
@@ -22,7 +22,7 @@ const Nosotros = () => {
                     </div>
                     <div className="nosotros__der">
                         <div className="wrap">
-                            <img src={imgValores}/>
+                            <img src={imgValores} />
                             <h5>Nuestros Valores</h5>
                             <ul>
                                 <li>Transparencia</li>
@@ -32,6 +32,18 @@ const Nosotros = () => {
                                 <li>Diligencia</li>
                                 <li>Constancia</li>
                             </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="nosotros__certificado__container">
+                    <div className="wrap">
+                        <div className="nosotros__certificado">
+                            <h3>CERTIFICADO N° 00297864</h3>
+                            <p>La Dirección de signos distintivos del Instituto Nacional de Defensa de la Competencia y de la Protección de la Propiedad Intelectual - INDECOPI, certifica que por mandato de la Resolución N° 017995-2020/DSD - INDECOPI de fecha 13 de octubre de 2020, ha quedado inscrito en el Registro de Marcas de Producto, el siguiente signo:</p>
+                            <li><span>Signo : </span>QR QUIMICA RODRÍGUEZ & FLORES SAC</li>
+                            <li><span>Distingue : </span>Preparaciones de productos de limpieza y disolventes</li>
+                            <li><span>Clase : </span>03 de la Clasificación Internacional</li>
+                            <li><span>Solicitud : </span>0857157-2020</li>
                         </div>
                     </div>
                 </div>
