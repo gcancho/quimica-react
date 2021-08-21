84bdfce31fd936db8d70da1a961e789eab40dd9f
gcancho <giovannicb19@gmail.com>
2021-08-20 22:59:08 -0500

    buscador funcionalidad ok, falta maquetar y dejarlo bonito

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -13,6 +13,38 @@ const Header = () => {
 
     const toogleMenu = () => cambiarMenu(!hola);
 
+    const buscador_interno = () => {
+        let bars_search = document.getElementById("ctn-bars-search");
+        let cover_ctn_search = document.getElementById("cover-ctn-search");
+        let inputSearch = document.getElementById("inputSearch");
+        let box_search = document.getElementById("box-search");
+
+        // alert('ga');
+        let filter = inputSearch.value.toUpperCase();
+        let li = box_search.getElementsByTagName("li");
+
+        //Recorriendo elementos a filtrar mediante los "li"
+        for (let i = 0; i < li.length; i++) {
+
+            let a = li[i].getElementsByTagName("a")[0];
+            let textValue = a.textContent || a.innerText;
+
+            if (textValue.toUpperCase().indexOf(filter) > -1) {
+
+                li[i].style.display = "";
+                box_search.style.display = "block";
+
+                if (inputSearch.value === "") {
+                    box_search.style.display = "none";
+                }
+
+            } else {
+                li[i].style.display = "none";
+            }
+
+        }
+    }
+
     return (
         <header className="header">
             <article className="header__arriba">
@@ -20,9 +52,23 @@ const Header = () => {
                 {/* Buscador */}
                 <div className="container__buscador__horario">
                     <div class="ctn-bars-search" id="ctn-bars-search">
-                        <input type="text" className="input-search" id="inputSearch" placeholder="¿Qué desea buscar?" />
+                        <input type="text" className="input-search" id="inputSearch" placeholder="¿Qué desea buscar?" onKeyUp={buscador_interno} />
                         <FontAwesomeIcon icon={faSearch} />
                     </div>
+                    <ul className="box-search" id="box-search">
+                        <li><a href="articulo.html"><i class="fas fa-search"></i><img src="img/img4.jpg" width="20px" />REACT</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>CSS</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>JavaScript</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>PHP</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>NODEJS</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>VUEJS</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>Como hacer</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>Como crear</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>Cloro</a></li>
+                        <li><a href="articulo.html"><i class="fas fa-search"></i>Limpia Vidrios</a></li>
+                    </ul>
+
+                    <div id="cover-ctn-search"></div>
                     {/*  */}
                     <div className="horario">
                         <div className="horario__icono">
@@ -30,10 +76,14 @@ const Header = () => {
                         </div>
                         <div className="horario__info">
                             <h6>Central</h6>
-                            <p>Lunes - Viernes:</p>
-                            <p>8:00 pm - 6:00 pm</p>
-                            <p>Sábado:</p>
-                            <p>8:00 pm - 12:00 pm</p>
+                            <div>
+                                <p>Lunes - Viernes:</p>
+                                <p>8:00 pm - 6:00 pm</p>
+                            </div>
+                            <div>
+                                <p>Sábado:</p>
+                                <p>8:00 pm - 12:00 pm</p>
+                            </div>
                             <p>994 268 798</p>
                             <p>955 310 163</p>
                         </div>
