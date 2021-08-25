3c55a80d9461fdf776547013b95ff080fec6bd29
gcancho <giovannicb19@gmail.com>
2021-08-25 12:02:00 -0500

    vista contacto principal

--- src/componentes/ContactoConsultas.js
+++ src/componentes/ContactoConsultas.js
@@ -1,9 +1,17 @@
+import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import ContactoConsultasItem from './ContactoConsultasItem';
 
 const ContactoConsultas = () => {
     return (
         <section className="contacto__consultas">
-
+            <p>Hola, tenemos diferentes canales para brindarte una correcta atención. ¿Qué consulta tienes hoy?</p>
+            <ContactoConsultasItem titulo="Servicio al cliente" listaDeseos={['- Tengo una consulta sobre el uso de un producto.','- Deseo presentar un reclamo.']}/> 
+            <ContactoConsultasItem titulo="Deseo contactar a un asesor comercial" listaDeseos={['– Soy una empresa y requiero contactar la asesoría.']}/> 
+            <ContactoConsultasItem titulo="Quiero ser parte de la red de distribuidores" listaDeseos={['– Deseo ser parte de la red de distribuidores.']}/> 
+            
         </section>
     );
 }
---
+++ src/componentes/ContactoConsultasItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const ContactoConsultasItem = ({ titulo, listaDeseos }) => {
+    return (
+        <article className="contacto__consultas__item">
+            <h3>{titulo}</h3>
+            <div className="container__consulta__boton">
+                <a><span>Realizar Consulta</span><FontAwesomeIcon icon={faArrowRight} /></a>
+            </div>
+            {listaDeseos.map((l, index) => <p key={index}>{l}</p>)}
+        </article>
+    );
+}
+
+export default ContactoConsultasItem;
